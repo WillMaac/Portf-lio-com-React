88521ea1356ece808c815ead3619b7a9bf87ae2b
WillMaac <anderson.wjj@hotmail.com>
2025-03-31 16:57:07 -0300

    Header finalizado

--- nome-do-projeto/src/components/Header/Header.jsx
+++ nome-do-projeto/src/components/Header/Header.jsx
@@ -1,9 +1,21 @@
+import Perfil from "../../assets/img/perfil.png";
+import styles from "./Header.module.css"
+
+
 export const Headeader = () => {
   return (
-    <section>
-      <div>
-        <h1>Hi, i´m ada</h1>
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Olá Mundo, meu nome é:</h1>
+        <p className={styles.title1}>Anderson De Jesus</p>
+        <p className={styles.description}>Front-End Web Developer</p>
+        <button className={styles.contactBtn}>
+          <a href="Willsouzza.333@gmail.com" >Contate-me</a>
+        </button>
       </div>
+      <img src={Perfil} alt= "Foto de Perfil" className={styles.fotoPerfil}/>
+      <div className={styles.topBlur}/>
+      <div className={styles.btn}/>
     </section>
   );
 };
